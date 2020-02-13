cd915c52d56b0e96deed053728c766c900d924ad
于志鹏 <yuzp1996@qq.com>
2020-02-13 18:24:08 +0800

    Update createfile.sh

--- createfile.sh
+++ createfile.sh
@@ -1,1 +1,1 @@
-i=1; while [ $i -le 99 ]; do name=`printf "test%02d.txt"  $i`; touch "$name"; i=$(($i+1)); done
+i=1; while [ $i -le 1000 ]; do name=`printf "test%02d.txt"  $i`; touch "$name"; i=$(($i+1)); done
